86d82e87e2cc49adb55c4f45840f9815834cdc75
Lethteria <Lethteria@gmail.com>
2017-06-21 23:19:47 +0300

    commit

---
+++ js/myJs.js
@@ -0,0 +1,31 @@
+var Hotel={
+    rooms: [{id:1,type:"standard"},
+             {id:2,type:"standard"},
+             {id:3,type:"standard"},
+             {id:4,type:"standard"},
+             {id:5,type:"standard"},
+             {id:6,type:"standard"},
+             {id:7,type:"standard"},
+             {id:8,type:"standard"},
+            {id:9,type:"luxe"},
+             {id:10,type:"luxe"},
+             {id:11,type:"luxe"},
+             {id:12,type:"luxe"},
+             {id:13,type:"luxe"},
+             {id:14,type:"luxe"},
+             {id:15,type:"luxe"},
+            {id:16,type:"economy"},
+             {id:17,type:"economy"},
+             {id:18,type:"economy"},
+             {id:19,type:"economy"},
+             {id:20,type:"economy"}]
+
+
+
+
+
+
+
+};
+console.log(Hotel.rooms.length);
+
